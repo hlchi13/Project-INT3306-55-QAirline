5b3db67ceb2e456493da704d3bf4251d51c19af6
hlchi13 <chihoang.uet@gmail.com>
2024-12-05 22:30:23 +0700

    Add initial project structure with React, React Router, and Tailwind CSS

---
+++ client/src/components/pages/HomePage.js
@@ -0,0 +1,14 @@
+import './HomePage.css';
+import {useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
+
+function HomePage() {
+
+    return (
+        <div class="home-container">
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default HomePage;
